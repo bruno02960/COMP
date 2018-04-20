9f647e30872b1975b5787f4f57bd0f5451f3cf5b
up201503995 <up201503995@fe.up.pt>
2018-04-20 19:08:00 +0100

    FunctionAnalysis e WhileAnalysis

--- YAL compiler/src/yal2jvm/SemanticAnalysis/FunctionAnalysis.java
+++ YAL compiler/src/yal2jvm/SemanticAnalysis/FunctionAnalysis.java
@@ -1,7 +1,6 @@
 package yal2jvm.SemanticAnalysis;
 
 import yal2jvm.Analysis;
-import yal2jvm.SymbolTables.VarSymbol;
 import yal2jvm.ast.*;
 
 import java.util.HashMap;
@@ -17,13 +16,18 @@ public class FunctionAnalysis extends Analysis
     @Override
     protected void parse()
     {
-
+        int astNumChilds = ast.jjtGetNumChildren();
+        for(int i = 0; i < astNumChilds; i++)
+        {
+            SimpleNode node = (SimpleNode) ast.jjtGetChild(i);
+            String nodeId = node.toString();
+            switch(nodeId)
+            {
+                case "ASTWHILE":
+                    WhileAnalysis whileAnalysis = new WhileAnalysis(ast, getUnifiedSymbolTable(), functionNameToFunctionSymbol);
+                    whileAnalysis.parse();
+            }
+        }
     }
 
-    private VarSymbol parseWhile() {
-        ASTEXPRTEST exprtest = ((ASTEXPRTEST) ast.jjtGetChild(0));
-
-        SimpleNode lhs = (SimpleNode) exprtest.jjtGetChild(0);
-        return parseLhs(lhs);
-    }
 }
--- YAL compiler/src/yal2jvm/SemanticAnalysis/WhileAnalysis.java
+++ YAL compiler/src/yal2jvm/SemanticAnalysis/WhileAnalysis.java
@@ -1,6 +1,26 @@
 package yal2jvm.SemanticAnalysis;
 
-public class WhileAnalysis
+import yal2jvm.Analysis;
+import yal2jvm.ast.ASTEXPRTEST;
+import yal2jvm.ast.SimpleNode;
+import yal2jvm.ast.Symbol;
+
+import java.util.HashMap;
+
+public class WhileAnalysis extends Analysis
 {
-    
+    public WhileAnalysis(SimpleNode ast, HashMap<String, Symbol> inheritedSymbols,
+                            HashMap<String, Symbol> functionNameToFunctionSymbolOfModule)
+    {
+        super(ast, inheritedSymbols, functionNameToFunctionSymbolOfModule);
+    }
+
+    @Override
+    protected void parse()
+    {
+        ASTEXPRTEST exprtest = ((ASTEXPRTEST) ast.jjtGetChild(0));
+
+        SimpleNode lhs = (SimpleNode) exprtest.jjtGetChild(0);
+        //return parseLhs(lhs);
+    }
 }
