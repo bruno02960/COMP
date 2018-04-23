e8509cc9800559ab96426329fa0ea0273e871f66
up201503995 <up201503995@fe.up.pt>
2018-04-23 21:51:58 +0100

    continued parseWhile

--- YAL compiler/src/yal2jvm/SemanticAnalysis/WhileAnalysis.java
+++ YAL compiler/src/yal2jvm/SemanticAnalysis/WhileAnalysis.java
@@ -1,11 +1,13 @@
 package yal2jvm.SemanticAnalysis;
 
 import yal2jvm.Analysis;
+import yal2jvm.SymbolTables.VarSymbol;
 import yal2jvm.ast.ASTEXPRTEST;
 import yal2jvm.ast.ASTSTATEMENTS;
 import yal2jvm.ast.SimpleNode;
 import yal2jvm.ast.Symbol;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class WhileAnalysis extends Analysis
@@ -22,10 +24,26 @@ public class WhileAnalysis extends Analysis
         ASTEXPRTEST exprTest = ((ASTEXPRTEST) ast.jjtGetChild(0));
         parseExprTest(exprTest);
 
+        //get symbols States Before While
+        ArrayList<Symbol> symbolsStatesBeforeWhile = (ArrayList<Symbol>) inheritedSymbols.values();
+
         ASTSTATEMENTS stmtlst = ((ASTSTATEMENTS) ast.jjtGetChild(1));
         parseStmtLst(stmtlst);
+
+        //get symbols States after While
+        ArrayList<Symbol> symbolsStatesAfterWhile = (ArrayList<Symbol>) inheritedSymbols.values();
+
+        //set as not initialized symbols that were initialized inside while, as its statements can not be executed
+        assert symbolsStatesBeforeWhile.size() == symbolsStatesAfterWhile.size();
+        for(int i = 0; i < symbolsStatesAfterWhile.size(); i++)
+        {
+            VarSymbol symbolBeforeWhile = (VarSymbol) symbolsStatesBeforeWhile.get(i);
+            VarSymbol symbolAfterWhile = (VarSymbol) symbolsStatesAfterWhile.get(i);
+            symbolAfterWhile.setType(symbolBeforeWhile.getType());
+        }
 
         //TODO: ver mySimbols (que sao do while e nao da funçao) e dai por ou nao como inicializado nos simbolos da funçao
         //TODO: aqui na realidade nao se por nenhum como initializado
     }
+
 }
