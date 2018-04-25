a57b6773c12ca0e0e8c79c747d5e8a916b02b5cc
up201503995 <up201503995@fe.up.pt>
2018-04-25 21:55:40 +0100

    removed size from VarSymbol

--- YAL compiler/src/yal2jvm/SymbolTables/VarSymbol.java
+++ YAL compiler/src/yal2jvm/SymbolTables/VarSymbol.java
@@ -6,22 +6,12 @@ public class VarSymbol extends Symbol
 {
     private String type;
     private boolean initialized;
-    private int size;
 
     public VarSymbol(String id, String type, boolean initialized)
     {
         super(id);
         this.type = type;
         this.initialized = initialized;
-        this.size = -1;
-    }
-
-    public VarSymbol(String id, String type, boolean initialized, int size)
-    {
-        super(id);
-        this.type = type;
-        this.initialized = initialized;
-        this.size = size;
     }
 
     public String getType()
@@ -44,19 +34,9 @@ public class VarSymbol extends Symbol
         this.initialized = initialized;
     }
 
-    public int getSize()
-    {
-        return size;
-    }
-
-    public void setSize(int size)
-    {
-        this.size = size;
-    }
-
     public VarSymbol getCopy()
     {
-        return new VarSymbol(new String(id), new String(type), new Boolean(initialized), new Integer(size));
+        return new VarSymbol(new String(id), new String(type), new Boolean(initialized));
     }
 
     @Override
