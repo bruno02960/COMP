57618ab342ea8202b47b5a66ac64a46bd1c81b8e
up201503995 <up201503995@fe.up.pt>
2018-06-02 16:29:34 +0100

    Removed double run, because it was putting another thing wrong

--- YAL compiler/src/yal2jvm/Yal2jvm.java
+++ YAL compiler/src/yal2jvm/Yal2jvm.java
@@ -96,12 +96,12 @@ public class Yal2jvm
         
         System.out.println("Initiating JVM code generation");
         HLIR hhir = new HLIR(ast);
-        if (this.optimize)
+        /*if (this.optimize)
             hhir.optimize();
         hhir.dataflowAnalysis();
         boolean allocated = hhir.allocateRegisters(this.localVars);
         if (!allocated)
-        	System.exit(-6);
+        	System.exit(-6);*/
 
         ArrayList<String> instructions = hhir.selectInstructions();
         String moduleName = hhir.getModuleName();
